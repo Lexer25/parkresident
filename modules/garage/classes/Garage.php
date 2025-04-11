30ca37c16f9f2fe67384ba99afff83b8499534b0
Andre <andrey.buharov@gmail.com>
2025-04-11 16:21:06 +0300

    Восстановил класс Garage, и все заработало штатно...

--- modules/garage/classes/Garage.php
+++ modules/garage/classes/Garage.php
@@ -2,73 +2,50 @@
 
 
 
-/* Place 
-07.04.2025
-Класс описывает ворота
+/* Garage 
+Класс для работы с сущностью Гараж
 
 */
 
 class Garage
 {
-			public $id; //id ворот
-            public $id_parking;//id парковки, которую обслуживают ворота
-            public $is_enter;//
-            public $name;//название
-            public $tablo_ip;// IP адрес контроллера табло 
-            public $tablo_port;// PORT контроллера табло 
-            public $box_ip;//IP адрес контроллера реле 
-            public $box_port;//PORT контоллера реле 
-            public $id_cam;//id камеры от cvs 
-            public $id_dev;// id точки проезда 
-            public $mode;// режим работы
-            public $messageIdlleTop;//надпись в режиме ожидания верхняя
-            public $messageIdlleDown;// надпись в режиме ожидания нижняя
+   private $result_ok='OK';
+	private $result_err='Err';
+	public $result;//результат выполнение метода OK - выполнен правильно, Err - выполнен с ошибкой
+	public $rdesc;// результат выполнения метода: набор данных или ошибок
+	
+	public $id;// id указатель на уникальной номер сущности
+	public $name;//имя сущности
+	public $is_present=false;//true - есть данные для указанного id, false - нет данных для указанного id
+	public $not_count=false;//1 - НЕ вести подсчет кол-ва свободных мест. NULL и другие значения - вести подсчет
+	public $div_code;//код уникальные идентификатор гаража
+	public $eventList;//список кодов событий, связанных с этим гаражом
+
 	
 	
 	
-	public function __construct($id=null)
+	 public function __construct($id=null)
     {
        if(!is_null($id))//если указан id, то создаю экземпляр класса с данными из БД.
 	   {
-	   $res=array();
-		$res=array();
-		$sql='select  
-			hlp.id,
-            hlp.id_parking,
-            hlp.is_enter,
-            hlp.name,
-            hlp.tablo_ip, 
-            hlp.tablo_port, 
-            hlp.box_ip, 
-            hlp.box_port, 
-            hlp.id_cam, 
-            hlp.id_dev, 
-            hlp.mode from HL_PARAM hlp
-			where hlp.id='.$id;
-		
-		$query = DB::query(Database::SELECT, $sql)
-			->execute(Database::instance('fb'))
-			->as_array();
-		$res=array();
-		foreach ($query as $key => $value)
+	   $this->id = $id;
+		$sql='select hln.name, hln.created, hln.not_count, hln.div_code from hl_garagename hln
+				where hln.id='.$this->id;
+		try
 		{
-			$this->id=Arr::get($value, 'ID');
-			$this->id_parking=Arr::get($value,'ID_PARKING');
-			$this->name=Arr::get($value,'NAME');
-			$this->is_enter=Arr::get($value,'IS_ENTER');
-			$this->tablo_ip=Arr::get($value,'TABLO_IP');
-			$this->tablo_port=Arr::get($value,'TABLO_PORT');
-			$this->box_ip=Arr::get($value,'BOX_IP');
-			$this->box_ip=Arr::get($value,'BOX_IP');
-			$this->box_port=Arr::get($value,'BOX_PORT');
-			$this->id_cam=Arr::get($value,'ID_CAM');
-			$this->id_dev=Arr::get($value,'ID_DEV');
-			$this->mode=Arr::get($value,'MODE');
-		}
-		return $res;	
+			$query = Arr::flatten(DB::query(Database::SELECT, $sql)
+			->execute(Database::instance('fb'))
+			->as_array());
+			$this->name=Arr::get($query, 'NAME');
+			$this->div_code=Arr::get($query, 'DIV_CODE');
+			$this->not_count=Arr::get($query, 'NOT_COUNT');
+			$this->is_present=true;
+			
+			//echo Debug::vars('23', $sql, $query); exit;
+			
 		} catch (Exception $e) {
 			////echo Debug::vars('30', $sql, $e->getMessage()); exit;
-			Log::instance()->add(Log::DEBUG, 'Line 40 '. $e->getMessage());
+			Log::instance()->add(Log::DEBUG, 'Line 52 '. $e->getMessage());
 		
 		}
 	   } else { // если не указан id, то создаю пустой экземпляр класса
@@ -76,29 +53,73 @@ class Garage
 	   }
 	}
 	
+	
+	/*
+	30.08.2023
+	получить список id событий по этому гаражу
+	*/
+	public function getEvents()
+	{
+		$sql='select first 1500 e.id from hl_orgaccess hlo
+			join people p on p.id_org=hlo.id_org
+			join card c on c.id_pep=p.id_pep and c.id_cardtype=4
+			join hl_events e on e.grz=c.id_card
+			join hl_eventcode ec on ec.id=e.event_code
+			where hlo.id_garage='.$this->id.'
+			and ec.id not in (46)
+			 order by e.id';
+		
+		
+		
+		$res=array();
+		try
+		{
+			$query = DB::query(Database::SELECT, $sql)
+			->execute(Database::instance('fb'))
+			->as_array();
+			foreach($query as $key=>$value)
+			{
+				//$res[]=$value;
+				$res[]=Arr::get($value, 'ID');
+			}
+			
+			return $res;
+		} catch (Exception $e) {
+			////echo Debug::vars('30', $sql, $e->getMessage()); exit;
+			Log::instance()->add(Log::DEBUG, 'Line 52 '. $e->getMessage());
+		
+		}
+	 
+	}
+	
+	
+	
 	/*
 	26.08.2023
 	Сохранение данных
 	*/
 	
-	public function add()
+	public function save()
 	{
-		//echo Debug::vars('61', $this->name);exit;
-		$sql='INSERT INTO HL_place (NAME)
-			values (\''.$this->name.'\')';
-			
-			
-		//echo Debug::vars('783', $sql);exit;
+		//echo Debug::vars('36', $this->name, $this->standalone);
+		 $sql=__('INSERT INTO TABLE (NAME, standalone)
+			VALUES (\':NAME\', :standalone)', 
+		array(
+			':NAME'=>$this->name,
+			':standalone'=>$this->standalone,
+			));
+	//echo Debug::vars('45', $sql); exit;
 		try
-				{
-				$query = DB::query(Database::INSERT, iconv('UTF-8','windows-1251',$sql))
+			{
+				$query = DB::query(Database::INSERT, iconv('UTF-8', 'CP1251',$sql))
 				->execute(Database::instance('fb'));
-				return true;
-				} catch (Exception $e) {
-					Log::instance()->add(Log::NOTICE, '794 '. $e->getMessage());
-					return false;
-				}
-		return;
+				$this->result=$this->result_ok;
+				$this->edesc='OK';
+			} catch (Exception $e) {
+				Log::instance()->add(Log::DEBUG, 'Line 83 '. $e->getMessage());
+				$this->result=$this->result_err;
+				$this->edesc=$e->getMessage();				
+			}
 	}
 	
 	/*
@@ -107,44 +128,39 @@ class Garage
 	*/
 	public function update()
 	{
-		//echo Debug::vars('36', $this);exit;
-		//echo Debug::vars('93', get_object_vars($this));//exit;
-		//hlr.id, hlr.name, hlr.is_active, hlr.created, hlr.modify
+		//echo Debug::vars('36', $this->name, $this->standalone);
 		
-		$sql='UPDATE HL_PLACE
+		$sql='UPDATE TABLE
 				SET NAME = \''.$this->name.'\',
-				PLACENUMBER = '.$this->placenumber.',
-				description = \''.$this->description.'\',
-				note = \''.$this->note.'\',
-				status = '.$this->status.',
-				id_parking = '.$this->id_parking.'
-			WHERE (ID = '.$this->id.')';
-		Log::instance()->add(Log::DEBUG, 'Line 101 '. $sql);//exit;
+				STANDALONE = '.$this->standalone.'
+			WHERE (ID_DEVTYPE = '.$this->id.') AND (ID_DB = 1)';
+		Log::instance()->add(Log::DEBUG, 'Line 101 '. $sql);
 		//echo Debug::vars('65', $sql); exit;
 		try
 			{
 			$query = DB::query(Database::UPDATE, iconv('UTF-8', 'CP1251',$sql))
 			->execute(Database::instance('fb'));
-			return true;
+			
+			$this->result=$this->result_ok;
+			$this->edesc=$this->id;
+			
 			} catch (Exception $e) {
 				Log::instance()->add(Log::DEBUG, 'Line 112 '. $e->getMessage());
-				
-				$this->mess=$e->getMessage();
-				return 	false;
+				$this->result=$this->result_err;
+				$this->edesc=$e->getMessage();				
 			}
 	}
 	
 	
 	/*
 	26.08.2023
-	Удаление указанного ЖК по его id
+	Удаление данных для указанного id
 	*/
-	public function del()
+	public function delete()
 	{
 		//echo Debug::vars('36', $this->name, $this->standalone);
 		
-		$sql='delete from hl_place
-			where id='.$this->id;
+		$sql='delete from ...';
 		Log::instance()->add(Log::DEBUG, 'Line 72 '. $sql);
 		//echo Debug::vars('65', $sql); exit;
 		try
@@ -152,12 +168,13 @@ class Garage
 			$query = DB::query(Database::DELETE, iconv('UTF-8', 'CP1251',$sql))
 			->execute(Database::instance('fb'));
 			
-			return true;
+			$this->result=$this->result_ok;
+			$this->edesc=$this->id;
 			
 			} catch (Exception $e) {
 				Log::instance()->add(Log::DEBUG, 'Line 139 '. $e->getMessage());
-				$this->mess=$e->getMessage();	
-				return true;				
+				$this->result=$this->result_err;
+				$this->edesc=$e->getMessage();				
 			}
 	}
 	
