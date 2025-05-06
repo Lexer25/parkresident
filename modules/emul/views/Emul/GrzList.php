ce8115525f87bd465b5d7d2a64ee0631f1245bb1
Lexer25 <b71@mail.ru>
2025-05-06 09:02:36 +0300

    Вместо POST сделал GET: это и компактнее, и нагляднее, и удобнее в обработке.

--- modules/emul/views/Emul/GrzList.php
+++ modules/emul/views/Emul/GrzList.php
@@ -81,28 +81,33 @@
 						foreach(array_slice(Arr::get($data, 'grzList'), 0, 3) as $key2=>$value2)
 						{
 							//echo Debug::vars('41', $value4);
-							echo Form::open('emul/test');
-								echo Arr::get($value2, 'GRZ');
+							/* echo Form::open('emul/test');
+								//echo Arr::get($value2, 'GRZ');
 								echo Form::hidden('card', Arr::get($value2, 'GRZ'));
 								echo Form::hidden('gate', Arr::get($value4, 'id'));
 								
-								echo Form::button('todo', 'IN', array('value'=>'in','class'=>'btn btn-success btn-xs', 'type' => 'submit')).
-								 Form::button('todo', 'OUT', array('value'=>'out','class'=>'btn btn-success btn-xs', 'type' => 'submit'));
-							echo Form::close();
+								//echo Form::button('todo', 'IN', array('value'=>'in','class'=>'btn btn-success btn-xs', 'type' => 'submit')).
+								//Form::button('todo', 'OUT', array('value'=>'out','class'=>'btn btn-success btn-xs', 'type' => 'submit'));
+								
+							echo Form::close(); */
+							 echo HTML::anchor('emul/test?card='.Arr::get($value2, 'GRZ').'&gate='.Arr::get($value4, 'id'), Arr::get($value2, 'GRZ'));
+							 echo '<br>';
 						}
 						//вывожу список CARD
 						foreach(array_slice(Arr::get($data, 'cardList'), 0, 3) as $key2=>$value2)
 						{
 							//echo Debug::vars('41', $value3);
-							echo Form::open('emul/test');
+							/* echo Form::open('emul/test');
 								echo Arr::get($value2, 'GRZ');
 								echo Form::hidden('card', Arr::get($value2, 'GRZ'));
 								echo Form::hidden('gate', Arr::get($value4, 'id'));
 								
 								echo Form::button('todo', 'IN', array('value'=>'in','class'=>'btn btn-success btn-xs', 'type' => 'submit')).
 								 Form::button('todo', 'OUT', array('value'=>'out','class'=>'btn btn-success btn-xs', 'type' => 'submit'));
-							echo Form::close();
+							echo Form::close(); */
 							
+							 echo HTML::anchor('emul/test?card='.Arr::get($value2, 'GRZ').'&gate='.Arr::get($value4, 'id'), Arr::get($value2, 'GRZ'));
+							 echo '<br>';
 						}
 						
 					echo '</td>';
@@ -117,159 +122,8 @@
 	</tbody>
 		
 </table>
-<table id="tablesorter" class="table table-striped table-hover table-condensed tablesorter">
 
-	<thead>
-		<tr>
-			<th><?php echo __('grz');?></th>
-			<th><?php echo __('pep_name');?></th>
-			<th><?php echo __('Активность');?></th>
-			<th><?php echo __('Категории доступа СКУД');?></th>
-			<th><?php echo __('Гараж');?></th>
-			<th><?php echo __('На парковке');?></th>
-			<th><?php echo __('Дата и время заезда');?></th>
-			<th><?php echo __('come_in_gate');?></th>
-			<th><?php echo __('come_out_gate');?></th>
-			<?php
-			
-				$list=Model::factory('Gates')->get_list_gate();
-				//echo Debug::vars('35', $list);exit;
-				foreach(Arr::get($list, 'res') as $key=>$value)
-				{
-					echo '<th>';
-						//echo Debug::vars('39', $value);//exit;
-						//echo $key;
-						//echo iconv('CP1251','UTF-8', Arr::get($value, 'name'));
-						echo Arr::get($value, 'name');
-					echo '</th>';
-				}
-			
-			?>
-			
-			
-		</tr>
-		
-		</thead>
-		<tbody>
-		<?php 
-		$i=1;
-		$checked='no';
-		$total_place=0;
-		$total_occup=0;
-		$total_vacant=0;
-			
-			
-		foreach($grz_list as $key=>$value)
-		{
-			echo '<tr>';
-				//echo '<td>'.$i++.'</td>';
-				echo '<td>'. HTML::anchor('grz/history/'.Arr::get($value,'ID_CARD'), iconv('windows-1251','UTF-8',Arr::get($value,'ID_CARD')));
-					if( preg_match("/[а-яё]/iu", iconv('windows-1251','UTF-8',Arr::get($value,'ID_CARD', '')))) echo '<br><span class="label label-danger">Русские буквы в ГРЗ</span>';
-					//echo Debug::vars('60', $value);
-					echo '</td>';
-				echo '<td>'.iconv('windows-1251','UTF-8', Arr::get($value,'GRZ_MODEL', '')).'</td>';
-				echo '<td>';
-				if(Arr::get($value,'ACTIVE', '0') == 1)
-					{				
-						echo '<span class="label label-success">Активен</span>';
-					} else 
-					{
-						echo '<span class="label label-danger">Не активен</span>';
-					}
-					echo '</td>';
-				//вывод списка категорий доступа
-				echo '<td>';
-				if(Arr::is_array(Arr::get($value,'accessList')))
-				{
-					foreach (Arr::get($value,'accessList') as $key1=>$value1)
-					{
-						echo iconv('windows-1251','UTF-8',Arr::get($value1, 'NAME', '')).'<br>';
-						
-					};
-				} else {
-						echo __('Нет');
-				}
-					echo '</td>';
-					
-				
-				
-				//вывод списка присвоенных гаражей
-				$_info=Arr::flatten(Arr::get($value,'garageList'));
-				$_garageName=iconv('windows-1251','UTF-8',Arr::get($_info, 'NAME'));
-				//$_garageName=Arr::get($_info, 'NAME');
-				echo '<td>';
-						echo HTML::anchor('garage/edit_garage/'.Arr::get($_info, 'ID_GARAGE'), iconv('windows-1251','UTF-8',Arr::get($_info, 'NAME')));
-				echo '</td>';
-					
-				
-				//на какой парковке находится
-				$_info=Arr::flatten(Arr::get($value,'inParking'));
-				echo '<td>';
-					echo ' '.iconv('windows-1251','UTF-8',Arr::get($_info, 'NAME'));
-					
-				echo '</td>';
-			
-			//время въезда на парковку		
-				echo '<td>';
-				
-					echo Arr::get($_info, 'ENTERTIME','');
-				echo '</td>';
-					
-				echo '<td>';	
-					
-					//echo Debug::vars('115', Arr::get($value, 'parkingList'));
-					foreach (Arr::get($value, 'parkingList') as $key1=>$value2)
-					{
-						
-						//echo Debug::vars('115', Arr::get($value2, 'ID_PARKING'), Arr::get($value2, 'PARKING_NAME'));
-						//echo Debug::vars('115', $value2);
-						
-					
-					echo Form::open('emul/car_in_parking');
-					echo Form::hidden('id_parking', Arr::get($value2,'ID_PARKING'));
-					//echo Debug::vars('100',Arr::get($value,'accessList') );
-					echo Form::button('car_in_parking', 'IN '.iconv('windows-1251','UTF-8', Arr::get($value2, 'PARKING_NAME')), array('value'=>Arr::get($value,'ID_CARD'),'class'=>'btn btn-success btn-xs', 'type' => 'submit'));
-					
-					echo Form::close();
-					}
-					
-				echo '</td>';
-					
-					echo Form::open('emul/car_out_parking');
-					
-				echo '<td>'.
-					Form::button('car_out_parking', 'OUT', array('value'=>Arr::get($value,'ID_CARD'),'class'=>'btn btn-danger btn-xs', 'type' => 'submit')).
-					'</td>';
-					echo Form::close();
-					
-					
-					echo Form::open('emul/test');
-				
-				foreach(Arr::get($list, 'res') as $key1=>$value1)
-				{
-					echo Form::hidden('card', Arr::get($value,'ID_CARD'));
-					echo Form::hidden('gate', Arr::get($value1, 'id'));
-					echo '<td>'.
-					Form::button('todo', 'IN', array('value'=>'in','class'=>'btn btn-success btn-sm', 'type' => 'submit')).
-					Form::button('todo', 'OUT', array('value'=>'out','class'=>'btn btn-success btn-sm', 'type' => 'submit')).
-					'</td>';
-					
-					
-				}
-				
 
-				
-				
-				
-			echo '</tr>';	
-			
-		}
-		
-		?>
-		</tbody>
-	</table>		
-	
-		<?php echo Form::close(); ?>
 </div>
 </div>
 <?php
