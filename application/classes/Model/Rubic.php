5fbaa965317e3387b85d879fafdd2a23100cf456
Lexer25 <b71@mail.ru>
2025-09-03 06:28:07 +0300

    несущественные изменения

--- application/classes/Model/Rubic.php
+++ application/classes/Model/Rubic.php
@@ -124,7 +124,7 @@ class Model_Rubic extends Model {
 		} 
 		
 			$sql.=' order by hle.event_time desc';
-		echo Debug::vars('71', date('Y-m-d H:i:s'), $data, $sql); exit;	
+		//echo Debug::vars('71', date('Y-m-d H:i:s'), $data, $sql); exit;	
 		//Log::instance()->add(Log::DEBUG, '106 '.$sql);
 		$query = DB::query(Database::SELECT, $sql)
 			->execute(Database::instance('fb'))
