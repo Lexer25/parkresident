0769e3e80294661ced0c1b917f6fe7064ad5ea12
Lexer25 <b71@mail.ru>
2025-12-14 10:26:53 +0300

    Сделал вывод информации о машиноместе без авторизации. изменение недоступно.

--- modules/place/views/Place/edit.php
+++ modules/place/views/Place/edit.php
@@ -3,61 +3,59 @@
 // страница для редактирования сущности
 //echo Debug::vars('4');exit;
 //echo Debug::vars('5', $place);//exit;
-echo Form::open('Place/control');
 
-if(Auth::Instance()->logged_in())
-{?>
-<script type="text/javascript">
-     
-  	$(function() {		
-  		$("#tablesorter").tablesorter({sortList:[[0,0]], headers: { 0:{sorter: false}}});
-  	});	
-	
-</script>
+?>
 <div class="panel panel-primary">
 	<div class="panel-heading">
 		<h3 class="panel-title"><?php echo __('Конфигурация машиноместа').' '. iconv('windows-1251','UTF-8',$place->name);
 		echo Form::hidden('id', $place->id);
-		
-		
-		
+
 		?></h3>
 	</div>
 	<div class="panel-body">
 		
 		<?php 
-		//echo __('Номер машиноместа').Form::input('placenumber', $place->placenumber).' '.__('(ID').$place->id.')<br>';
-		echo Form::hidden('placenumber', $place->placenumber).'<br>';
-		echo __('(ID').$place->id.')<br>';
-		//echo __('Номер машиноместа: ').$place->placenumber.'<br>';
-		//echo __('Название машиноместа').Form::input('name', iconv('windows-1251','UTF-8', $place->name), array('maxlength'=>50)).'<br>';
-		//echo __('Статус').Form::input('status', iconv('windows-1251','UTF-8', $place->status), array('maxlength'=>50)).'<br>';
-		$parking=new Parking($place->id_parking);
-		//получить список паркингов.
+			
+			$parking=new Parking($place->id_parking);
+			$parkingPlace=Model::factory('ParkingPlace')->get_list_for_select();//получил список id жилых комплексов
 		
-		//вывести список паркингов
+
+		if(Auth::Instance()->logged_in())
+		{
+			echo Form::open('Place/control');
+			echo Form::hidden('placenumber', $place->placenumber).'<br>';
+			echo __('(ID').$place->id.')<br>';
 		
-		// сохранить изменения. Однако может быть коллизия, если номер машиноместа уже используется.
-		//вывод: переносить машиноместа из паркинга в паркинг нельзя!!!
-		//список парковочных площадок для выбора
-		$parkingPlace=Model::factory('ParkingPlace')->get_list_for_select();//получил список id жилых комплексов
-		//echo Debug::vars('42', $parkingPlace);exit;
-		$selectList=array();
-		echo 'Парковочная площадка: '.Form::select('id_parking', $parkingPlace, $place->id_parking).' '.__('(ID').$place->id_parking.')';
-		echo '<br>';
-		echo __('Название').Form::input('name', iconv('windows-1251','UTF-8', $place->name), array('maxlength'=>50));
-		echo '<br>';
-		echo __('Описание').Form::input('description', iconv('windows-1251','UTF-8', $place->description), array('maxlength'=>50)).'<br>';
-		echo __('Описание').Form::input('note', iconv('windows-1251','UTF-8', $place->note), array('maxlength'=>50)).'<br>';
-		
+			
+			$selectList=array();
+			echo 'Парковочная площадка: '.Form::select('id_parking', $parkingPlace, $place->id_parking).' '.__('(ID').$place->id_parking.')';
+			echo '<br>';
+			echo __('Название').Form::input('name', iconv('windows-1251','UTF-8', $place->name), array('maxlength'=>50));
+			echo '<br>';
+			echo __('Описание').Form::input('description', iconv('windows-1251','UTF-8', $place->description), array('maxlength'=>50)).'<br>';
+			echo __('Описание2').Form::input('note', iconv('windows-1251','UTF-8', $place->note), array('maxlength'=>50)).'<br>';
+			
+			?>
+				<?php
+			echo Form::button('todo', Kohana::message('rubic','rubic_change_config'), array('value'=>'update','class'=>'btn btn-success', 'type' => 'submit'));	
+		} else {
+		echo 'Парковочная площадка: '. Arr::get($parkingPlace, $place->id_parking);
+			echo '<br>';
+			echo __('Название :name', array('name'=>$place->name));
+			echo '<br>';
+			echo __('Описание :name', array('name'=>iconv('windows-1251','UTF-8', $place->description)));
+			echo '<br>';
+			echo __('Описание2 :name', array('name'=>iconv('windows-1251','UTF-8', $place->note)));
+			echo '<br>';
+			echo __('Авторизуйтесь для редактирования.');
+		}
 		?>
-			<?php
-		echo Form::button('todo', Kohana::message('rubic','rubic_change_config'), array('value'=>'update','class'=>'btn btn-success', 'type' => 'submit'));	
-		?>
+		
+		
 	</div>
 </div>
 
-<?php }?>
+
 
 
 
