0f7a88a2b9fdfb63cc910a7f652a228784bd82eb
Lexer25 <b71@mail.ru>
2025-12-14 11:16:33 +0300

    Перевел вывод информации о машиноместе в табличной форме, и получил ошибку при сохранении.
    надо вернуться и посмотреть как было до этих изменений.

--- modules/place/views/Place/edit.php
+++ modules/place/views/Place/edit.php
@@ -18,37 +18,60 @@
 			
 			$parking=new Parking($place->id_parking);
 			$parkingPlace=Model::factory('ParkingPlace')->get_list_for_select();//получил список id жилых комплексов
-		
 
-		if(Auth::Instance()->logged_in())
-		{
 			echo Form::open('Place/control');
 			echo Form::hidden('placenumber', $place->placenumber).'<br>';
 			echo __('(ID').$place->id.')<br>';
-		
-			
 			$selectList=array();
-			echo 'Парковочная площадка: '.Form::select('id_parking', $parkingPlace, $place->id_parking).' '.__('(ID').$place->id_parking.')';
+			/* echo 'Парковочная площадка: '.Form::select('id_parking', $parkingPlace, $place->id_parking).' '.__('(ID').$place->id_parking.')';
 			echo '<br>';
 			echo __('Название').Form::input('name', iconv('windows-1251','UTF-8', $place->name), array('maxlength'=>50));
 			echo '<br>';
 			echo __('Описание').Form::input('description', iconv('windows-1251','UTF-8', $place->description), array('maxlength'=>50)).'<br>';
 			echo __('Описание2').Form::input('note', iconv('windows-1251','UTF-8', $place->note), array('maxlength'=>50)).'<br>';
+			 */
+			echo __('
+			<table class="table table-striped table-hover table-condensed">
+				<tr>
+					<th>:header1</th>
+					<th>:header2</th>
+				</tr>
+				<tr>
+					<td>:name1</td>
+					<td>:value1</td>
+				</tr>
+				<tr>
+					<td>:name2</td>
+					<td>:value2</td>
+				</tr>
+				<tr>
+					<td>:name3</td>
+					<td>:value3</td>
+				</tr>
+				<tr>
+					<td>:name4</td>
+					<td>:value4</td>
+				</tr>
+				
+				
+			</table>', array(
+				':header1'=>'Параметр',
+				':header2'=>'Значение',
+				':name1'=>'Парковочная площадка',
+				':value1'=>Form::select('id_parking', $parkingPlace, $place->id_parking).' '.__('(ID').$place->id_parking.')',
+				':name2'=>'Название',
+				':value2'=>Form::input('name', iconv('windows-1251','UTF-8', $place->name), array('maxlength'=>50)),
+				':name3'=>'Описание',
+				':value3'=>Form::input('description', iconv('windows-1251','UTF-8', $place->description), array('maxlength'=>50)),
+				':name4'=>'Описание2',
+				':value4'=>Form::input('note', iconv('windows-1251','UTF-8', $place->note), array('maxlength'=>50)),
+				
+				
+				
+				));
 			
-			?>
-				<?php
-			echo Form::button('todo', Kohana::message('rubic','rubic_change_config'), array('value'=>'update','class'=>'btn btn-success', 'type' => 'submit'));	
-		} else {
-		echo 'Парковочная площадка: '. Arr::get($parkingPlace, $place->id_parking);
-			echo '<br>';
-			echo __('Название :name', array('name'=>$place->name));
-			echo '<br>';
-			echo __('Описание :name', array('name'=>iconv('windows-1251','UTF-8', $place->description)));
-			echo '<br>';
-			echo __('Описание2 :name', array('name'=>iconv('windows-1251','UTF-8', $place->note)));
-			echo '<br>';
-			echo __('Авторизуйтесь для редактирования.');
-		}
+			if(Auth::Instance()->logged_in()) echo Form::button('todo', Kohana::message('rubic','rubic_change_config'), array('value'=>'update','class'=>'btn btn-success', 'type' => 'submit'));	
+		echo Form::close();	
 		?>
 		
 		
@@ -57,8 +80,3 @@
 
 
 
-
-
-
-
-<?echo Form::close();?>	
